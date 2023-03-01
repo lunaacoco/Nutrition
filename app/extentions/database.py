e8a0a563f42918d9df0a7ab9c8644b49d3df5ffe
Laura Emily Schiebilski <emily.schiebilski@code.berlin>
2023-03-01 10:06:02 +0100

    added user model

--- app/extentions/database.py
+++ app/extentions/database.py
@@ -4,3 +4,9 @@ from flask_migrate import Migrate
 
 migrate = Migrate()
 db = SQLAlchemy()
+
+class CRUDMixin():
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
---
+++ app/users/models.py
@@ -0,0 +1,6 @@
+from app.extentions.database import db, CRUDMixin
+
+class User(db.Model, CRUDMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(128), index=True, unique = True)
+    password = db.Column(1024)
